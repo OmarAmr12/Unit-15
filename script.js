3f0b5c367c0bbada595a535dc3b1b1fbeffef481
OmarAmr12 <1594439@stu.tmc.ac.uk>
2026-05-14 15:25:32 +0100

    Add form validation function

---
+++ script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+  let name = document.getElementById("name").value;
+
+  if (name == "") {
+    alert("Please enter your name");
+    return false;
+  }
+
+  alert("Booking successful!");
+  return true;
+}
