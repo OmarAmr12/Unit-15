35719d489d6b0f51b28e805fff0ae470593d38d6
OmarAmr12 <1594439@stu.tmc.ac.uk>
2026-06-03 14:11:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+function goBooking() {
+    window.location.href = "booking.html";
+}
+
 function validateForm() {
-  let name = document.getElementById("name").value;
+    let name = document.getElementById("name").value;
 
-  if (name == "") {
-    alert("Please enter your name");
-    return false;
-  }
-
-  alert("Booking successful!");
-  return true;
+    if (name == "") {
+        alert("Please complete all fields");
+    } else {
+        alert("Booking confirmed!");
+    }
 }
