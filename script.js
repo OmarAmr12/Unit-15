cad969d0fdc54200eb7702dfeb60d6787b147034
OmarAmr12 <1594439@stu.tmc.ac.uk>
2026-06-09 11:23:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Redirects user to booking page
 function goBooking() {
     window.location.href = "booking.html";
 }
-
+// Validates booking form input
 function validateForm() {
     let name = document.getElementById("name").value;
 
@@ -11,11 +12,11 @@ function validateForm() {
         alert("Booking confirmed!");
     }
 }
-
+// Increases text size for accessibilit
 function increaseText() {
     document.body.style.fontSize = "20px";
 }
-
+// Decreases text size for accessibility
 function decreaseText() {
     document.body.style.fontSize = "14px";
 }
